3d1595c369f7cb3813c2d1702aeb201579fa2e0b
ronnies007 <38360146+ronnies007@users.noreply.github.com>
2020-05-22 17:00:40 +0200

    Update NN_config.py

--- NN_config.py
+++ NN_config.py
@@ -6,7 +6,7 @@ log_file = "training_log.txt"
 # Parameters for the model and dataset.
 training_size = 55040   # number of question/answer pairs to be generated before every epoch
 digits = int(3)         # how many max. digits allowed in single number
-reverse = False         # this was valid for only using addition in the original code
+reverse = False         # this was True for only using addition in the original code
 hidden_size_1 = 2048    # num of lstm layer dimension
 hidden_size_2 = 2048    # I just played around with these numbers. after adding more math to the questions 
                             # ... it needed more neurons.
