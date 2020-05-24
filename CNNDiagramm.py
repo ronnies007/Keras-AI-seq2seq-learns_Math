b2399641006b1f64548fa5c6a1b6c6f41ede04f2
ronnies007 <38360146+ronnies007@users.noreply.github.com>
2020-05-24 12:42:03 +0200

    Update CNNDiagramm.py

--- CNNDiagramm.py
+++ CNNDiagramm.py
@@ -39,8 +39,8 @@ fig = plt.figure(figsize=(7, 4))
 fig.fontsize = 9
 latest = 400 #1440 #int(cfg.latest)
 
-states_path = cfg.log_path # 'R:/temp/' 
-log_file = cfg.log_file
+states_path = 'R:/temp/' 
+log_file = cfg.log_file2
 sw = True
 f=0
 ## ---------- wait for data file to fill ------------
@@ -62,9 +62,7 @@ lastRun = int(df['batch'].max())
 dmaxVal, dminVal = getMinMax(df)
 # ax1.xaxis_date()
 print ("dmaxVal, dminVal:", dmaxVal,",", dminVal)
-
-zoomX1, zoomX2, zoomY1, zoomY2 = -100, lastRun + 10, dminVal, dmaxVal    # specify the limits
-zoomX1, zoomX2, zoomY1, zoomY2 = -10, lastRun + 10, dminVal, dmaxVal  # specify the limits
+zoomX1, zoomX2, zoomY1, zoomY2 = -10, lastRun + len(df)/10, dminVal, dmaxVal  # specify the limits
 xview = lastEpisode - df['batch'].iloc[-2]
 lastEpisode_old = lastEpisode
 lastRun_old = lastRun
@@ -75,21 +73,22 @@ def on_xlims_change(axes):
     global ax1, zoomX1, zoomX2, zoomY1, zoomY2, sw
     zoomX1, zoomX2 = ax1.get_xlim()
     sw = True
-    print ("updated xlims: ", zoomX1, zoomX2)
+    # print ("updated xlims: ", zoomX1, zoomX2)
 
 
 def on_ylims_change(axes):
     global ax1, zoomX1, zoomX2, zoomY1, zoomY2, sw
     zoomY1, zoomY2 = ax1.get_ylim()
     sw = True
-    print ("updated ylims: ", zoomY1, zoomY2)
+    # print ("updated ylims: ", zoomY1, zoomY2)
 
 
 def resetView():
     global ax1, zoomX1, zoomX2, zoomY1, zoomY2, plt, sw, lastEpisode, dmaxVal, dminVal, df, states_path, log_file
     df = pd.read_csv(str(states_path + log_file), sep=",", header=0, encoding="utf8", low_memory=False) # , parse_dates=True)
+    lastRun = int(df['batch'].max())
     dmaxVal, dminVal = getMinMax(df)
-    zoomX1, zoomX2, zoomY1, zoomY2 = -10, lastRun + 10, dminVal, dmaxVal  # specify the limits
+    zoomX1, zoomX2, zoomY1, zoomY2 = -10, lastRun + len(df)/10, dminVal, dmaxVal  # specify the limits
     sw = False
     plt.xlim(zoomX1, zoomX2)
     plt.ylim(zoomY1, zoomY2)
@@ -105,26 +104,14 @@ def press(event):
     if event.key == ' ':
         resetView()
 
-
 def animate(i):
-    global dminVal, lastRun_old, lastRun, dmaxVal, qlive, ax1, zoomX1, zoomX2, zoomY1, zoomY2, plt, df, sw, xview, lastEpisode_old, states_path, lastEpisode, log_file
-    print(df.columns)
-    # ax1 = fig.add_subplot(1,1,1)
-    # ax1.xaxis_date()
-    #plt.savefig('test.pdf')
-    # ax1.set_xlim(xlim)
-    # ax1.set_ylim(ylim)
+    global dminVal, lastRun_old, lastRun, dmaxVal, qlive, ax1, zoomX1, zoomX2, zoomY1, zoomY2, plt, df, sw, xview, lastEpisode_old, states_path, lastEpisode, log_file 
     df = pd.read_csv(str(states_path + log_file), sep=",", header=0, encoding="utf8", low_memory=False) #, parse_dates=True)
-
+    print(df.columns)
     lastEpisode = int(df['batch'].max()) #int(df['epoch'].iloc[-1])
-    
     lastRun = int(df['batch'].max())
-    # dmaxVal, dminVal = getMinMax(df)
-    # zoomX1, zoomX2, zoomY1, zoomY2 = -10, lastRun + 10, dminVal, dmaxVal  # specify the limits
-    
     xview = int(lastEpisode - df['batch'].iloc[-2])   
     print (xview)   
-
     latest = len(df.batch) # to shrink the diagramm (not used)
     #reduce to latest factor
     if len(df.batch) > int(latest):        # Drop 
@@ -136,50 +123,44 @@ def animate(i):
 
     fig.clear()
     ax1 = fig.add_subplot(1,1,1)
-
     ax1.set_xlabel("batch")    #x-achse set
     ax1.set_ylabel("loss")  #y-achse set
     ax1.yaxis.grid(color='gray', linestyle='dashed') # backgr. grid
     ax1.xaxis.grid(color='gray', linestyle='dashed') # backgr. grid
     #ax1.autoscale_view()
     ax1.set_facecolor((0.8, 0.8, 0.8))   # backgr. color grey
-
-    plt.xlim(zoomX1, zoomX2)
-    plt.ylim(zoomY1, zoomY2)
-
-    ax1.set_title('seq2seq training state  @trained batch '+ str(int(np.max(df.batch)+2)) )
-    for label in ax1.xaxis.get_ticklabels(): label.set_rotation(20)  #x-achse rotate annot.
-
-    plt.tight_layout()
-
-    print ("lastEpisode: ", lastEpisode)
-
-    if (sw==False):        #  no zoomt or movement = reset
+    lastRun = int(df['batch'].iloc[-1])
+    if (sw==False):        #  no zoom or movement = reset
         if not (lastRun_old == lastRun): 
             dmaxVal, dminVal = getMinMax(df)   
-            zoomX1, zoomX2, zoomY1, zoomY2 = -10, lastRun + 10, dminVal, dmaxVal  # specify the limits  
+            zoomX1, zoomX2, zoomY1, zoomY2 = -10, lastRun + len(df)/10, dminVal, dmaxVal  # specify the limits  
             lastRun_old = lastRun
-            #xview = int(df['epoch'].iloc[-1]) - int(df['epoch'].iloc[-2]) 
             plt.xlim(int(zoomX1), int(zoomX2))
             plt.ylim(zoomY1, zoomY2)
             zoomX1, zoomX2 = ax1.get_xlim()
-            #plt.show()
         else:
             dmaxVal, dminVal = getMinMax(df)   
             plt.xlim(zoomX1, zoomX2)
             plt.ylim(dminVal, dmaxVal)
 
     elif (sw==True):      # with zoom or movement
-        if not (lastRun_old == lastRun):              
-            lastRun_old = lastRun
-            xview = int(df['batch'].iloc[-1]) - int(df['batch'].iloc[-2]) 
+        if not (lastRun_old == lastRun):   
+            xview = lastRun - lastRun_old            
+            # xview = int(df['batch'].iloc[-1]) - int(df['batch'].iloc[-2]) 
             plt.xlim(int(zoomX1+xview), int(zoomX2+xview))
             plt.ylim(zoomY1, zoomY2)
             zoomX1, zoomX2 = ax1.get_xlim()
-            #plt.show()
+            lastRun_old = lastRun
         else:
             plt.xlim(zoomX1, zoomX2)
             plt.ylim(zoomY1, zoomY2)
+
+    plt.xlim(zoomX1, zoomX2)
+    plt.ylim(zoomY1, zoomY2)
+    ax1.set_title('seq2seq training state  @trained batch '+ str(int(np.max(df.batch)+2)) )
+    for label in ax1.xaxis.get_ticklabels(): label.set_rotation(20)  #x-achse rotate annot.
+    plt.tight_layout()
+    print ("lastEpisode: ", lastEpisode)
     
     plot1, = plt.plot(df.index, df['loss'], label="loss.", marker="", picker=3, linewidth=1)
     plot2, = plt.plot(df.index, df['accuracy'], label="acc.", marker="", picker=3, linewidth=1)
